e9bcac6263f00d502f4d93cd149b9356fea8b388
DangDacTu <dagdtus@gmail.com>
2026-03-09 11:57:21 +0700

    delete all code in main

--- social-network/frontend/src/api/postApi.js
+++
@@ -1,9 +0,0 @@
-import axiosClient from './axiosClient';
-
-export const createPost = (data) => {
-  return axiosClient.post("/posts", data);
-};
-
-export const getPosts = () => {
-  return axiosClient.get("/posts");
-};
